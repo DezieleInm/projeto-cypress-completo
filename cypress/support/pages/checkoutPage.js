cc69d90b9bdd04a2f471f281d598dc6eca3fe459
DezieleInm <deziele.silva@inmetrics.com.br>
2024-12-02 12:48:35 -0300

    Update checkoutPage.js

--- cypress/support/pages/checkoutPage.js
+++ cypress/support/pages/checkoutPage.js
@@ -1,15 +1,18 @@
 const checkoutElements = require("../locators/checkoutElements")
 
 class checkoutPage {
-
+ // Clica no botão de checkout
+    
     clickBtnCheckout() {
         cy.get(checkoutElements.btnCheckout).click()
     }
-
+ // Valida se a visão do resumo do pedido está visível
+    
     validateOrderPaymentView() {
         cy.get(checkoutElements.orderPaymentView).should('be.visible')
     }
-
+  // Valida se o produto está visível no resumo do pedido
+    
     validateProductPaymentView() {
         cy.get(checkoutElements.productToBeValidated).should('be.visible')
     }
