0d3bad9fd886309454149341c489116317ba25ef
DezieleInm <deziele.silva@inmetrics.com.br>
2024-12-02 12:51:41 -0300

    Update cartElements.js

--- cypress/support/locators/cartElements.js
+++ cypress/support/locators/cartElements.js
@@ -1,17 +1,22 @@
 class cartElements {
+
+// Botão de adicionar ao carrinho
     
     get btnAddToCart() {
         return '.fixedBtn > .roboto-medium';
     }
-
+// Botão para ir para o carrinho
+    
     get btnGoToCart() {
         return '#shoppingCartLink';
     }
-
+// Botão de checkout no popup
+    
     get btnCheckoutPopup() {
         return '#checkOutPopUp';
     }
-
+// Elemento de item adicionado ao carrinho
+    
     get itemAdded() {
         return 'a > h3.ng-binding'
     }
