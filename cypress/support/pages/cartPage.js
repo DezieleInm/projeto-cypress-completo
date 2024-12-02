ac68556b000e9501f3f97a2ee64b1c9981bfc978
DezieleInm <deziele.silva@inmetrics.com.br>
2024-12-02 12:46:35 -0300

    Update cartPage.js

--- cypress/support/pages/cartPage.js
+++ cypress/support/pages/cartPage.js
@@ -9,18 +9,22 @@ class cartPage {
     }
     
 // Vai para a página do carrinho
+    
     clickGoToCart() {
         cy.get(cartElements.btnGoToCart).click()
     }
 // Valida se o popup de checkout foi exibido
+    
     validateAddedToCart() {
         cy.get(cartElements.btnCheckoutPopup).should('be.visible')
     }
 // Valida se o item foi adicionado ao carrinho
+    
     validateItemAdded() {
         cy.get(cartElements.itemAdded).should('be.visible')
     }
 // Clica no popup de checkout
+    
     clickToCheckoutPopup() {
         cy.get(cartElements.btnCheckoutPopup).click()
     }
