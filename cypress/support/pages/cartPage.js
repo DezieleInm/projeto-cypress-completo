a67f5b9df81abfeeb68e139618d951d418755212
DezieleInm <deziele.silva@inmetrics.com.br>
2024-12-02 12:46:11 -0300

    Update cartPage.js

--- cypress/support/pages/cartPage.js
+++ cypress/support/pages/cartPage.js
@@ -2,22 +2,25 @@ const cartElements = require("../locators/cartElements")
 
 class cartPage {
 
+// Adiciona um item ao carrinho
+
     addingToCart() {
         cy.get(cartElements.btnAddToCart).click()
     }
-
+    
+// Vai para a página do carrinho
     clickGoToCart() {
         cy.get(cartElements.btnGoToCart).click()
     }
-
+// Valida se o popup de checkout foi exibido
     validateAddedToCart() {
         cy.get(cartElements.btnCheckoutPopup).should('be.visible')
     }
-
+// Valida se o item foi adicionado ao carrinho
     validateItemAdded() {
         cy.get(cartElements.itemAdded).should('be.visible')
     }
-
+// Clica no popup de checkout
     clickToCheckoutPopup() {
         cy.get(cartElements.btnCheckoutPopup).click()
     }
