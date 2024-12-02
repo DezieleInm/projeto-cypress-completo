b3d350ccd173423afd24c30150b9fb62517c54d0
DezieleInm <deziele.silva@inmetrics.com.br>
2024-12-02 12:49:36 -0300

    Update homePage.js

--- cypress/support/pages/homePage.js
+++ cypress/support/pages/homePage.js
@@ -1,12 +1,12 @@
 const homeElements = require('../locators/homeElements')
 
 class homePage {
-
+  // Visita a página inicial e valida a visibilidade do logo
     visitHomePage() {
         cy.visit("https://advantageonlineshopping.com/");
         cy.get(homeElements.logoHomepage).should('be.visible')
     }
-
+  // Pesquisa por um produto
     typeToSearch(product) {
         cy.get(homeElements.btnSearch).click()
         cy.get(homeElements.textFieldSearch).type(product).type('{enter}')
