e2759d38c5818d25f8dd84219f934cda07447666
DezieleInm <deziele.silva@inmetrics.com.br>
2024-12-02 12:54:20 -0300

    Update searchElements.js

--- cypress/support/locators/searchElements.js
+++ cypress/support/locators/searchElements.js
@@ -1,9 +1,10 @@
 class searchElements {
-
+ // Contagem de itens encontrados
+    
     get titleItemsCount() {
         return '.titleItemsCount';
     }
-
+  // Item pesquisado (usando um seletor mais específico)
     get searchedItem() {
         return ':nth-child(1) > :nth-child(4) > .productName';
     }
