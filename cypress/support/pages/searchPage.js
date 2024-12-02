f0115e488c250d9c142acd613962336cd144df18
DezieleInm <deziele.silva@inmetrics.com.br>
2024-12-02 12:43:58 -0300

    Update searchPage.js

--- cypress/support/pages/searchPage.js
+++ cypress/support/pages/searchPage.js
@@ -2,14 +2,19 @@ const searchElements = require("../locators/searchElements")
 
 class searchPage {
 
+ // Valida se o título da quantidade de itens retornados está visível
+    
     validateListReturned() {
         cy.get(searchElements.titleItemsCount).should('be.visible')
     }
 
+// Valida se o item pesquisado está visível
+    
     validateItemReturned() {
         cy.get(searchElements.searchedItem).should('be.visible')
     }
-
+// Clica no item retornado da pesquisa
+    
     clickItemReturned() {
         cy.get(searchElements.searchedItem).click()
     }
