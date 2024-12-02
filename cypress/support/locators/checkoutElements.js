294751dff78648d33704aa6982a3921a2bca9c47
DezieleInm <deziele.silva@inmetrics.com.br>
2024-12-02 12:52:46 -0300

    Update checkoutElements.js

--- cypress/support/locators/checkoutElements.js
+++ cypress/support/locators/checkoutElements.js
@@ -1,13 +1,13 @@
 class checkoutElements {
-    
+// Botão de checkout   
     get btnCheckout() {
         return '#checkOutButton';
     }
-
+// Visão do pagamento do pedido
     get orderPaymentView() {
         return '#orderPayment';
     }
-
+// Produto a ser validado no carrinho 
     get productToBeValidated() {
         return '#userCart > #toolTipCart > :nth-child(1) > table > tbody > #product > :nth-child(2) > a > h3.ng-binding';
     }
